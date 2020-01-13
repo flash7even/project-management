51c2d1cac63435def6e0f0c032460789d3a6d096
flash7even <tarangokhan77@gmail.com>
2020-01-14 00:24:16 +0600

    Update access codes for project and bill services

--- method lookup upload script.py
+++ method lookup upload script.py
@@ -3,40 +3,23 @@ import requests
 data = [
 ["CREATE_ATTENDANCE", "Create Attendance Log", "ATTENDANCE"],
 
-["CREATE_DEVICE", "Create Device", "DEVICE"],
-["DELETE_DEVICE", "Delete Device", "DEVICE"],
-["UPDATE_DEVICE", "Update Device Details", "DEVICE"],
-["SEARCH_DEVICE", "Search Device Entry", "DEVICE"],
-["VIEW_DEVICE", "View Device Details", "DEVICE"],
+["CREATE_PROJECT", "Create Project", "PROJECT"],
+["DELETE_PROJECT", "Delete Project", "PROJECT"],
+["UPDATE_PROJECT", "Update Project Details", "PROJECT"],
+["SEARCH_PROJECT", "Search Project Entry", "PROJECT"],
+["VIEW_PROJECT", "View Project Details", "PROJECT"],
 
-["CREATE_DOOR", "Create Door", "DOOR"],
-["DELETE_DOOR", "Delete Door", "DOOR"],
-["UPDATE_DOOR", "Update Door Details", "DOOR"],
-["SEARCH_DOOR", "Search Door Entry", "DOOR"],
-["VIEW_DOOR", "View Door Details", "DOOR"],
-
-["SEARCH_FACE", "Search FRS Matching", "FRS"],
-["ENROLL_FACE", "Enroll Image Face", "FRS"],
-
-["UPLOAD_FILE", "Upload File", "FILES"],
-["DOWNLOAD_FILE", "Download File", "FILES"],
+["CREATE_BILL", "Create Bill", "BILL"],
+["DELETE_BILL", "Delete Bill", "BILL"],
+["UPDATE_BILL", "Update Bill Details", "BILL"],
+["SEARCH_BILL", "Search Bill Entry", "BILL"],
+["VIEW_BILL", "View Bill Details", "BILL"],
 
 ["CREATE_LOOKUP", "Create Lookup Entry", "LOOKUP"],
 ["DELETE_LOOKUP", "Delete Lookup", "LOOKUP"],
 ["UPDATE_LOOKUP", "Update Lookup Details", "LOOKUP"],
 ["SEARCH_LOOKUP", "Search Lookup Entry", "LOOKUP"],
 ["VIEW_LOOKUP", "View Lookup Details", "LOOKUP"],
-
-["CREATE_GUEST", "Create Guest", "GUEST"],
-["DELETE_GUEST", "Delete Guest", "GUEST"],
-["UPDATE_GUEST", "Update Guest Details", "GUEST"],
-["SEARCH_GUEST", "Search Guest Entry", "GUEST"],
-["VIEW_GUEST", "View Guest Details", "GUEST"],
-
-["UPDATE_APPOINTMENT", "Update Appointment Details", "APPOINTMENT"],
-["SEARCH_APPOINTMENT", "Search Appointment Entry", "APPOINTMENT"],
-["DELETE_APPOINTMENT", "Delete Appointment", "APPOINTMENT"],
-["CREATE_APPOINTMENT", "Create Appointment", "APPOINTMENT"],
 
 ["CREATE_USER", "Create User", "USER"],
 ["DELETE_USER", "Delete User", "USER"],
@@ -49,8 +32,8 @@ data = [
 ["DEPRECATED", "DEPRECATED", "GLOBAL"]
 ]
 
-host_name = "192.168.5.127:9200"
-_es_index = "tardy_method_access_lookup"
+host_name = "localhost:9200"
+_es_index = "pms_method_access_lookup"
 _es_type = "access"
 _http_headers = {'Content-Type': 'application/json'}
 rs = requests.session()
